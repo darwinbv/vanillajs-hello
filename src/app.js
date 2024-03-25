683f8b5c6b884a5b045dd5ad7f74b5ebd54629a4
darwinbv <162520814+darwinbv@users.noreply.github.com>
2024-03-25 04:42:55 +0000

    Final del proyecto

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  let randomnumber = Math.random() * 10;
+  console.log("Hello Rigo from the console! " + randomnumber);
+};
+
+// Esta funcion genera una escusa aleatoria
+let generateExcuse = () => {
+  let pronoun = ["A", "the"];
+  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let possetion = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let possetionindx = Math.floor(Math.random() * possetion.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    "" +
+    subject[subjIndx] +
+    "" +
+    action[actionIndx] +
+    "" +
+    possetion[possetionindx] +
+    "" +
+    where[whereIndx]
+  );
 };
